ac9186d0a2096783e0e0e5a23152db39af997817
JohnElbogen <john.elbogen@snhu.edu>
2021-05-24 21:00:04 -0400

    module3
    module3

---
+++ module3/app_server/controllers/travel.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+var fs = require('fs');
+
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+/* GET travel view */
+const travel = (req, res) => {
+    res.render('travel', { title: 'Travlr Getaways', trips });
+};
+
+module.exports = {
+    travel
+};
