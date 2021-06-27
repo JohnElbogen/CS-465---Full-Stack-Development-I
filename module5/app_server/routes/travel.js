4cbc7faf094ec09a3f39c63a2285a875e04a7236
JohnElbogen <john.elbogen@snhu.edu>
2021-06-27 17:37:22 -0400

    module5

---
+++ module5/app_server/routes/travel.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/travel');
+
+/* GET home page. */
+router.get('/', controller.travelList);
+
+module.exports = router;
