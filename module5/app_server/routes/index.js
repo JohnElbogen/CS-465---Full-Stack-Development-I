cb1c1330de74c003cdcee530cadeafd3c5964270
JohnElbogen <john.elbogen@snhu.edu>
2021-06-27 17:46:34 -0400

    Revert "module5"
    This reverts commit 4cbc7faf094ec09a3f39c63a2285a875e04a7236.

--- module5/app_server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/main');
-
-/* GET home page. */
-router.get('/', controller.index);
-
-module.exports = router;
--- module5/app_server/routes/travel.js
+++
@@ -1,9 +0,0 @@
-// JavaScript source code
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/travel');
-
-/* GET home page. */
-router.get('/', controller.travelList);
-
-module.exports = router;
